7a5b001dfd5fc1f03d812ca6e84a2866270db6b4
Ryve <91507517+ryvexc@users.noreply.github.com>
2023-06-18 00:42:48 +0700

    unnecessary folders

--- database/urlcontroller.ts
+++
@@ -1,9 +0,0 @@
-export default function manipulateURL(url: string, title: string): void {
-  if (typeof history.replaceState != "undefined") {
-    var obj = {
-      Title: title,
-      Url: url,
-    };
-    history.replaceState(obj, obj.Title, obj.Url);
-  }
-}
