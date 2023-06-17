e1cf714efbf0b74b83e4ad040d9a50d6647ed53e
ryvexc <arimasword1906@gmail.com>
2023-06-17 22:15:23 +0700

    test api

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,5 +1,13 @@
 import { NextApiHandler, NextApiResponse } from "next";
+import clientPromise from "@/lib/mongodb";
 
-module.exports = (req: NextApiHandler, res: NextApiResponse) => {
-  res.status(200).send("hi! this is arif");
+module.exports = async (req: NextApiHandler, res: NextApiResponse) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("ryve_store");
+
+    res.status(200).json(await db.collection("accounts").find({}).toArray());
+  } catch (e) {
+    console.log(e);
+  }
 };
