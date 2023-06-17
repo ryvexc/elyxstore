76cf66af6ee293f03c985d37f71b33c4479b6df9
ryvexc <arimasword1906@gmail.com>
2023-06-18 01:33:04 +0700

    adding font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <title>Elyx Store</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
